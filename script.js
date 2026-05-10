259c71a8234d336ece72653dbd73f543f1ab1102
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-10 12:42:18 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const navbar =
+document.getElementById("navbar");
 
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
